20e1cae06afa8f3136721c32db0a143817beab20
Phelipe Chiarelli <phelipechiarelli@gmail.com>
2022-09-27 23:30:34 -0300

    novos ajustes de UI

--- src/app/pages/providers/providers.module.ts
+++ src/app/pages/providers/providers.module.ts
@@ -3,15 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { ProvidersRoutingModule } from './providers-routing.module';
 import { NewProviderComponent } from './new-provider/new-provider.component';
+import { ProviderFormComponent } from 'src/app/components/provider-form/provider-form.component';
+
 
 
 @NgModule({
   declarations: [
-    NewProviderComponent
+    NewProviderComponent,
+    ProviderFormComponent
   ],
   imports: [
     CommonModule,
     ProvidersRoutingModule
+    
   ]
 })
 export class ProvidersModule { }
